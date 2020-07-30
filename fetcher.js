d440573f3a801a53e19d03935a68ec5222a5c459
TimaJuma <tima.xpl@gmail.com>
2020-07-30 06:20:45 +0000

    fetcher

---
+++ fetcher.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+const fs = require('fs');
+
+
+
+let urls = process.argv.slice(2); 
+
+
+const fileName = 'hello.html'
+
+request(`${urls[0]}${urls[1]}`, (error, response, body) => {
+  //console.log('error:', error); // Print the error if one occurred
+
+  const status = response && response.statusCode;
+  const htmlBody = body;
+  fs.writeFile(fileName, htmlBody, function(err) {
+    if (err) return console.log(err);
+    let size = getSize(fileName);
+    console.log(`Downloaded and saved ${size} bytes to ./${fileName}`)
+
+  })
+  // console.log('statusCode:', response && response.statusCode); 
+  // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+
+
+//helper function get size of file
+const getSize = function(fname) {
+  let stats = fs.statSync(fname);
+  let fileSize = stats['size'];
+  return fileSize;
+}
